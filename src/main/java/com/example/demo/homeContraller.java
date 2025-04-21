c31b3862b165cf665de0d81da9be7181bbee7b3e
mahmoudaligh <mahmoudvhj@gmail.com>
2025-04-21 20:28:46 +0200

    Update homeContraller.java

--- src/main/java/com/example/demo/homeContraller.java
+++ src/main/java/com/example/demo/homeContraller.java
@@ -12,18 +12,4 @@ public class homeContraller {
     public String getIndex(){
         return "index.html";
     }
-
-    @GetMapping("/login")
-    public String getLogin(){
-        return "login.html";
-    }
-
-    @GetMapping("/register")
-    public String getRegister(){
-        return "register.html";
-    }
-    @GetMapping("/base")
-    public String getBase(){
-        return "base.html";
-    }
 }
